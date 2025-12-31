2def44c31e8ebd73169d097d14be74b2d89697f0
kalkidan <kalkidanmehiretjemberie@gmail.com>
2025-12-31 17:28:28 +0300

    Add Firebase Storage service for file upload and deletion

---
+++ app/lib/services/storage_service.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> uploadFile({required File file, required String path}) async {
+    final ref = _storage.ref().child(path);
+    final uploadTask = await ref.putFile(file);
+    return await uploadTask.ref.getDownloadURL();
+  }
+
+  Future<void> deleteFile(String url) async {
+    final ref = _storage.refFromURL(url);
+    await ref.delete();
+  }
+}
